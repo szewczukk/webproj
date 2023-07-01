049b3499b78388fa20ad2eb465ced02ddc169c64
szewczukk <dmitro.szewczuk@gmail.com>
2023-07-01 13:38:55 +0200

    Scroll into element with an offset

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hello, world!")
+const anchors = document.querySelectorAll('a[href]')
+const navHeight = document
+    .querySelector("nav")
+    .getBoundingClientRect()
+    .height
+
+for (const anchor of anchors) {
+
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault()
+
+        const targetTop = document
+            .querySelector(this.hash)
+            .getBoundingClientRect()
+            .top;
+        const top = targetTop + window.pageYOffset - navHeight;
+
+        window.scrollTo({
+            top,
+            left: 0,
+            behavior: "smooth",
+        })
+    })
+}
+
